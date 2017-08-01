595f1b6b48c6956aa3fe12e2a60203f251f5ab22
Hari Rao <hari.rao@outlook.com>
2017-08-01 23:25:29 +0100

    Adding a scala test

---
+++ src/test/java/com/services/FeeCalculatorServiceImplTestScalaVersion.scala
@@ -0,0 +1,20 @@
+package com.services
+
+import java.util
+
+import org.junit.Test
+import org.junit.Assert._
+
+/**
+  * Created by Hari Rao on 01/08/17.
+  */
+class FeeCalculatorServiceImplTestScalaVersion {
+
+  @Test
+  def emptyOneCreate  {
+       val stack = new util.ArrayList[Int]
+       assertTrue(stack.isEmpty)
+  }
+
+
+}
